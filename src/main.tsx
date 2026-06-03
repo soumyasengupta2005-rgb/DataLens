f794e2094e3883a8f4d92db0742a33d2967e2519
Soumya <soumya.ckd2005@gmail.com>
2026-06-03 23:18:07 +0530

    analytics addition

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
-
+import { Analytics } from "@vercel/analytics/react";
 import "pagedjs";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>
+    <Analytics />
+  </StrictMode>,
 );
